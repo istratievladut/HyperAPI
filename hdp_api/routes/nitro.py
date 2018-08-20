29954c3e88e3b0c014ca9e897cfc1986d0e779bb
CedricCazinHC <cedric.cazin@bearingpoint.com>
2018-08-20 12:49:31 +0200

    Add a new route
    Change a route path
    Change routes method

--- hdp_api/routes/nitro.py
+++ hdp_api/routes/nitro.py
@@ -6,7 +6,7 @@ class Nitro(Resource):
 
     class _getForecasts(Route):
         name = "getForecasts"
-        httpMethod = Route.GET
+        httpMethod = Route.POST
         path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts"
         _path_keys = {
             'project_ID': Route.VALIDATOR_OBJECTID,
@@ -26,7 +26,7 @@ class Nitro(Resource):
     class _insertForecast(Route):
         name = "insertForecast"
         httpMethod = Route.POST
-        path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts"
+        path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts/add"
         _path_keys = {
             'project_ID': Route.VALIDATOR_OBJECTID,
             'dataset_ID': Route.VALIDATOR_OBJECTID
@@ -54,7 +54,7 @@ class Nitro(Resource):
 
     class _getForecastTunes(Route):
         name = "getForecastTunes"
-        httpMethod = Route.GET
+        httpMethod = Route.POST
         path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts/{forecast_ID}/tunes"
         _path_keys = {
             'project_ID': Route.VALIDATOR_OBJECTID,
@@ -72,10 +72,20 @@ class Nitro(Resource):
             'forecast_ID': Route.VALIDATOR_OBJECTID
         }
 
-    class _getForecastAggregateTunes(Route):
-        name = "getForecastAggregateTunes"
-        httpMethod = Route.GET
-        path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts/{forecast_ID}/tunes/aggregate"
+    class _getForecastTunesAggregateGeo(Route):
+        name = "getForecastTunesAggregateGeo"
+        httpMethod = Route.POST
+        path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts/{forecast_ID}/tunes/aggregate/geo"
+        _path_keys = {
+            'project_ID': Route.VALIDATOR_OBJECTID,
+            'dataset_ID': Route.VALIDATOR_OBJECTID,
+            'forecast_ID': Route.VALIDATOR_OBJECTID
+        }
+
+    class _getForecastTunesAggregateDepot(Route):
+        name = "getForecastTunesAggregateDepot"
+        httpMethod = Route.POST
+        path = "/nitro/projects/{project_ID}/datasets/{dataset_ID}/forecasts/{forecast_ID}/tunes/aggregate/depot"
         _path_keys = {
             'project_ID': Route.VALIDATOR_OBJECTID,
             'dataset_ID': Route.VALIDATOR_OBJECTID,
