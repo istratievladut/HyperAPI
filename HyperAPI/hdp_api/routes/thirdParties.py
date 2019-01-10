e4f1fbd17250a4b37259e7a23e29696bd8c96c4f
serkan.sahin <serkan.sahin210@gmail.com>
2019-01-10 15:18:17 +0100

    hyperapi now handles thirdparties route starting from HDP version 1.0

--- HyperAPI/hdp_api/routes/thirdParties.py
+++ HyperAPI/hdp_api/routes/thirdParties.py
@@ -1,11 +1,9 @@
 from HyperAPI.hdp_api.routes import Resource, Route
-from HyperAPI.hdp_api.routes.base.version_management import available_since
 
 
 class ThirdParties(Resource):
     name = "3rdParties"
 
-    @available_since('3.4')
     class _get3rdPartiesSettings(Route):
         name = "get3rdPartiesSettings"
         httpMethod = Route.GET
